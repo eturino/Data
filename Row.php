e45b66bd16aeb8ef87fe1daa042d831ad654d660
Eduardo Turiño <eturino@gmail.com>
2012-06-22 00:59:31 +0200

    observable row bugfix + Data_Row fix on insert

--- Row.php
+++ Row.php
@@ -12,4 +12,19 @@ class EtuDev_Data_Row extends EtuDev_Data_ObservableRow {
 	}
 
 
+	/**
+	 * @return EtuDev_Data_PseudoArray
+	 */
+	public function _setDefaultDataWhenNull() {
+		$this->setValuesOnlyIfNull($this->_getDefaultData());
+		return $this;
+	}
+
+	protected function _insert() {
+		var_dump($this->_data, $this->_modifiedFields);
+		$this->_setDefaultDataWhenNull();
+		var_dump('POST',$this->_data, $this->_modifiedFields);
+		return parent::_insert();
+	}
+
 }
