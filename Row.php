77a830dab39540c2f5aff91eba210df5dae2633f
Eduardo Turiño <eturino@gmail.com>
2012-08-05 22:24:54 +0200

    row check

--- Row.php
+++ Row.php
@@ -11,6 +11,43 @@ class EtuDev_Data_Row extends EtuDev_Data_ObservableRow implements EtuDev_Data_H
 		return array();
 	}
 
+	/**
+	 * returns the $data parameter if it is a valid row of table $table, or creates a row with that info
+	 *
+	 * @static
+	 *
+	 * @param EtuDev_Data_Table $table
+	 * @param                   $data
+	 *
+	 * @return array|EtuDev_Interfaces_ToArrayAble|EtuDev_Interfaces_ToArrayAbleFull|null
+	 */
+	static public function checkOrCreateRowOfTable(EtuDev_Data_Table $table, $data) {
+		if (!$data) {
+			return null;
+		}
+
+		if ($table->isRow($data)) {
+			return $data;
+		}
+
+		$c = null;
+
+		if (is_array($data)) {
+			/** @var $data array */
+			$c = $table->createRow();
+			$c->setFromArray($data);
+		} elseif (($data instanceof EtuDev_Interfaces_ToArrayAbleFull) || method_exists($data, 'toArrayFull')) {
+			/** @var $data EtuDev_Interfaces_ToArrayAbleFull */
+			$c = $table->createRow();
+			$c->setFromArray($data->toArrayFull());
+		} elseif (($data instanceof EtuDev_Interfaces_ToArrayAble) || method_exists($data, 'toArray')) {
+			/** @var $data EtuDev_Interfaces_ToArrayAble */
+			$c->setFromArray($data->toArray());
+			$c = $table->createRow();
+		}
+
+		return $c ? : $data;
+	}
 
 	/**
 	 * @return EtuDev_Data_PseudoArray
@@ -45,11 +82,12 @@ class EtuDev_Data_Row extends EtuDev_Data_ObservableRow implements EtuDev_Data_H
 
 	/**
 	 * @param array|SolrObject|Traversable $data
+	 *
 	 * @return EtuDev_Data_Row
 	 */
-	public function setHighlightData($data){
+	public function setHighlightData($data) {
 		$a = array();
-		foreach($data as $field => $value) {
+		foreach ($data as $field => $value) {
 			$a[$field] = $value[0];
 		}
 
@@ -58,25 +96,25 @@ class EtuDev_Data_Row extends EtuDev_Data_ObservableRow implements EtuDev_Data_H
 	}
 
 
-	public function isHighlighted($origkey){
+	public function isHighlighted($origkey) {
 		$key = $this->getDefinedAlias($origkey);
 		return $this->_isHighlighted($key);
 	}
 
-	protected function _isHighlighted($key){
+	protected function _isHighlighted($key) {
 		return @isset($this->_highlightData[$key]);
 	}
 
-	public function getDirectHighlighted($origkey){
+	public function getDirectHighlighted($origkey) {
 		$key = $this->getDefinedAlias($origkey);
 		return $this->_getDirectFromHighlightedData($key);
 	}
 
-	protected function _getDirectFromHighlightedData($key){
+	protected function _getDirectFromHighlightedData($key) {
 		return @$this->_highlightData[$key];
 	}
 
-	static public function getIdField(){
+	static public function getIdField() {
 		return 'id';
 	}
 
