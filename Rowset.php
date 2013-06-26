81159c23710658f0b6a49cc18a7580cf44b03b92
Eduardo Turiño <eturino@gmail.com>
2013-06-26 12:13:11 +0200

    refresh keys, append rows

--- Rowset.php
+++ Rowset.php
@@ -9,10 +9,7 @@ class EtuDev_Data_Rowset extends Zend_Db_Table_Rowset
     public function __construct(array $config)
     {
         parent::__construct($config);
-        if ($this->_data) {
-            $this->firstKey = key(array_slice($this->_data, 0, 1, true));
-            $this->endKey   = key(array_slice($this->_data, -1, 1, true));
-        }
+        $this->refreshKeys();
     }
 
     /**
@@ -48,4 +45,73 @@ class EtuDev_Data_Rowset extends Zend_Db_Table_Rowset
         return null;
     }
 
+    protected $total;
+
+    public function getTotal()
+    {
+        if (is_null($this->total)) {
+            $table = $this->getTable();
+            if ($table instanceof EtuDev_Data_Table) {
+                $this->total = $table->getFoundRows();
+            }
+        }
+        return $this->total;
+    }
+
+    /**
+     * @param array|Zend_Db_Table_Rowset $list_rows
+     */
+    public function appendRows($list_rows)
+    {
+        foreach ($list_rows as $v) {
+            if (is_array($v)) {
+                $this->doAppendRowData($v);
+            } elseif ($v instanceof Zend_Db_Table_Row_Abstract) {
+                $this->doAppendRowData($v->toArray());
+            }
+        }
+        $this->refreshCount();
+        $this->refreshRows();
+    }
+
+    public function appendRow(Zend_Db_Table_Row $data)
+    {
+        $this->doAppendRowData($data->toArray());
+        $this->refreshCount();
+        $this->refreshRows();
+    }
+
+    public function appendRowData(array $data)
+    {
+        $this->doAppendRowData($data);
+        $this->refreshCount();
+        $this->refreshRows();
+    }
+
+    protected function doAppendRowData(array $data)
+    {
+        $this->_data[] = $data;
+    }
+
+    protected function refreshCount()
+    {
+        $this->_count = count($this->_data);
+    }
+
+    protected function refreshRows()
+    {
+        if (count($this->_rows) != count($this->_data)) {
+            return $this->setTable($this->_table);
+        }
+
+        return $this->_connected;
+    }
+
+    protected function refreshKeys()
+    {
+        if ($this->_data) {
+            $this->firstKey = key(array_slice($this->_data, 0, 1, true));
+            $this->endKey   = key(array_slice($this->_data, -1, 1, true));
+        }
+    }
 }
